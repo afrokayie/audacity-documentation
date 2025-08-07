c097057a56ed173e765a091371a9ef72bac7a9e3
afrokayie <oluwolekayode61@gmail.com>
2025-08-07 15:26:58 +0100

    updated social card

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -80,7 +80,7 @@ const config = {
       },
     ],
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/Group2.svg',
       navbar: {
         title: 'AudaCity',
         logo: {
